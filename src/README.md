c729b5f42b4b22c387114051f1f51eb35832d27f
morgan bergen <morgan@ku.edu>
2023-02-01 22:00:56 -0600

    added links to c src

--- src/README.md
+++ src/README.md
@@ -13,3 +13,8 @@ In EECS388 labs, you will learn how to use microcontrollers and embedded process
 **lab assignments**
 
 1. [lab01](./lab01/README.md)
+    - [01 program - calculator](./src/lab01/src/01-program/main.c)
+    - [02 program - height categorizor](./src/lab01/src/02-program/main.c)
+    - [03 program - prime numbers](./src/lab01/src/03-program/main.c)
+
+
--- src/lab01/src/03-program/main.c
+++ src/lab01/src/03-program/main.c
@@ -11,32 +11,46 @@
 #include <stdio.h>
 
 int main() {
+    
+    /* cp   check prime
+     * 
+     * this will be used as a boolean variable to check if a value within
+     * the following loops are prime or not
+     * currently cp is undefined
+     */
+    int cp;
 
-    int limit = 0;
+    /* initialization   i = 2, starts at 2
+     * condition        i < 100, stops looping at 99
+     * increment        i + 1, increments by 1 after each loop
+     */
+    for (int i = 2; i < 100; i++) {
+        /* check prime is initialized as true
+         * we start with assuming that it's true
+         */
+        cp = 1; 
 
-    printf("enter limit of primes: ");
-    scanf("%d", &limit);
+        /* initialization   j = 2, starts at 2
+         * condition        j < i, loops until j sees all of the the ith integers
+         * increment        j + 1, increments by 1 after each loop
+         */
+        for (int j = 2; j < i; j++) {
 
-    printf("%d%s", limit, "\n");
+            // if i/j has a no remainder then it is not a prime
+            if (i % j == 0) {
 
-    for (int i = 2; i <= limit; i++) {
-        printf("%d", i);
-        printf("is looked at \n");
-        for (int j = 2; (j % i == 0); j++) {
-            printf("%d", i);
-            printf("if prime \n");
+                // check prime initialized back to false
+                cp = 0; 
+                // we break out of the inner for loop
+                break;
+            }
+        }
+        // if check prime is true print it, otherwise increment i and reloop
+        if (cp == 1) {
+            printf("prime: %d\n", i);
         }
     }
 
-
-
+    // end of program
     return(0);
 }
-/*
-    for (int i = 0; i < limit; i++) {
-        for (int j = 0; j % i != 0; j++) {
-            printf("%d", j);
-        }
-    }
-*/
-

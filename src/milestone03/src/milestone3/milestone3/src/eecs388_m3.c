10e683673b0e0b5e02ca4aa571c31a156f6b5984
morgan bergen <morgan@ku.edu>
2023-05-04 08:56:38 -0500

    latest version eecs388_m3

--- src/milestone03/src/milestone3/milestone3/src/eecs388_m3.c
+++ src/milestone03/src/milestone3/milestone3/src/eecs388_m3.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
-#include <stdlib.h>
 #include "eecs388_lib.h"
 #include "metal/i2c.h"
 
+
 struct metal_i2c *i2c;
-uint8_t bufWrite[5];
+uint8_t bufWrite[9];
 uint8_t bufRead[1];
 
-//The entire setup sequence
-void set_up_I2C()
-{
+
+//The entire setup sequencebigNum
+void set_up_I2C() {
     uint8_t oldMode;
     uint8_t newMode;
     _Bool success;
 
+
     bufWrite[0] = PCA9685_MODE1;
     bufWrite[1] = MODE1_RESTART;
     printf("%d\n",bufWrite[0]);
-    
+   
     i2c = metal_i2c_get_device(0);
 
-    if(i2c == NULL){
+    if (i2c == NULL) {
         printf("Connection Unsuccessful\n");
     }
-    else{
+    else {
         printf("Connection Successful\n");
     }
-    
-    //Setup Sequence
+   
+    // setup sequence
     metal_i2c_init(i2c,I2C_BAUDRATE,METAL_I2C_MASTER);
     success = metal_i2c_write(i2c,PCA9685_I2C_ADDRESS,2,bufWrite,METAL_I2C_STOP_DISABLE);//reset
     delay(100);
@@ -39,7 +39,7 @@ void set_up_I2C()
     bufWrite[0] = PCA9685_MODE1;//Address
     success = metal_i2c_transfer(i2c,PCA9685_I2C_ADDRESS,bufWrite,1,bufRead,1);//initial read
     printf("Read success: %d and control value is: %d\n", success, bufWrite[0]);
-    
+   
     //Configuring Control 1
     oldMode = bufRead[0];
     newMode = (oldMode & ~MODE1_RESTART) | MODE1_SLEEP;
@@ -50,6 +50,7 @@ void set_up_I2C()
     bufWrite[0] = PCA9685_PRESCALE;//Setting PWM prescale
     bufWrite[1] = 0x79;
     success = metal_i2c_write(i2c,PCA9685_I2C_ADDRESS,2,bufWrite,METAL_I2C_STOP_DISABLE);//sets prescale
+                                                                                         //
     bufWrite[0] = PCA9685_MODE1;
     bufWrite[1] = 0x01 | MODE1_AI | MODE1_RESTART;
     printf("on setting is %d\n", bufWrite[1]);
@@ -59,42 +60,170 @@ void set_up_I2C()
     bufWrite[0] = PCA9685_MODE1;
     success = metal_i2c_transfer(i2c,PCA9685_I2C_ADDRESS,bufWrite,1,bufRead,1);//initial read
     printf("Set register is %d\n",bufRead[0]);
-} 
 
-void breakup(int bigNum, uint8_t* low, uint8_t* high)
-{
-// your code from Milestone 1
 }
 
-void steering(int angle)
-{
-// your code from Milestone 1
+
+/* defining the breakup function
+ * task 1: breaking 12 bit into two 8-bit define the function created that recieves a 12 bit number,
+ * 0 to 4096 and breaks it up into two 8 bit numbers. assign these values to a referenced
+ * value handed into the function.
+ *
+ * ex:
+ * uint8_t variable1;
+ * uint8_t variable2;
+ * breakup(2000,&variable1,&variable2);
+ * variable1 -> low 8 bits of 2000
+ * variable2 -> high 8 bits of 2000
+ */   
+
+void breakup(int bigNum, uint8_t* low, uint8_t* high){
+
+    *low = bigNum & 0x00ff;
+    *high = (bigNum >> 8) & 0xff;
 }
 
-void stopMotor()
-{
-// your code from Milestone 1
+/*
+ * changing Steering Heading
+ * task 2: using getServoCycle(), bufWrite, bufRead, breakup(), and and metal_i2c_transfer(),
+ * implement the function defined above to control the servo by sending it an angle ranging
+ * from -45 to 45. use the getServoCycle function to get the value to breakup.
+ *
+ * ex:
+ * int valToBreak = getServoCycle(45);
+ * >>>sets valToBreak to 355
+ *
+ * note: the motor's speed controller is either LED0 or LED1 depending on where its plugged into
+ * the board. If its LED1, simply add 4 to the LED0 address
+ * ex: steering(0); -> driving angle forward
+ */
+
+void steering(int angle){
+
+    int cycleVal = getServoCycle(angle);
+    bufWrite[0] = PCA9685_LED0_ON_L+4;
+    bufWrite[1] = 0;
+    bufWrite[2] = 0;
+    breakup(cycleVal, &bufWrite[3], &bufWrite[4]);
+    metal_i2c_transfer(i2c, PCA9685_I2C_ADDRESS, bufWrite, 5, bufRead, 1);
 }
 
-void driveForward(uint8_t speedFlag)
-{
-// your code from Milestone 1
+/*
+ * motor config/stop. This will cause a second beep upon completion
+ * task 3: using bufWrite, bufRead, breakup(), and metal_i2c_transfer(), implement the
+ * funcion made above. This function configure the motor by writing a value of 280 to the motor.
+ * include a 2 second delay after calling this function in order to calibrate
+ *
+ * note: the motor's speed controller is either LED0 or LED1 depending on where its plugged into
+ * the board. If its LED1, simply add 4 to the LED0 address
+ * ex: stopMotor();
+ */
+
+void stopMotor() {
+
+    breakup(280, &bufWrite[3], &bufWrite[4]);
+    bufWrite[0] = PCA9685_LED0_ON_L ;
+    bufWrite[1] = 0;
+    bufWrite[2] = 0;
+    metal_i2c_transfer(i2c,PCA9685_I2C_ADDRESS,bufWrite,5,bufRead,1);
+}
+/*
+ * ###########################################################################################
+ * ATTENTION: The following section will cause the wheels to move. confirm that the robot
+ * is propped up to avoid it driving away, as well as that nothing is touching the wheels
+ * and can get caught in them if anything goes wrong, unplug the hifive board from the
+ * computer to stop the motors from moving avoid sticking your hand inside the car while
+ * its wheels are spinning
+ * ###########################################################################################
+ */
+
+/*
+ * motor forward
+ * task 4: using bufWrite, bufRead, breakup(), and metal_i2c_transfer(), implement the function
+ * made above to Drive the motor forward. the given speedFlag will alter the motor speed as follows
+ *
+ * speedFlag = 1 -> value to breakup = 313
+ * speedFlag = 2 -> value to breakup = 315 (optional)
+ * speedFlag = 3 -> value to breakup = 317 (optional)
+ *
+ * note 1: the motor's speed controller is either LED0 or LED1 depending on where its plugged into
+ * the board. If its LED1, simply add 4 to the LED0 address or type and replace LED1 with LED0
+ * ex: driveForward(1);
+ */
+
+void driveForward(int speedFlag){
+
+    bufWrite[0] = PCA9685_LED0_ON_L;
+    bufWrite[1] = 0;
+    bufWrite[2] = 0;
+
+    if (speedFlag == 1) {
+
+        breakup(313, &bufWrite[3], &bufWrite[4]);
+
+    } else if (speedFlag == 2) {
+
+        breakup(315, &bufWrite[3], &bufWrite[4]);
+
+    } else if (speedFlag == 3) {
+
+        breakup(317, &bufWrite[3], &bufWrite[4]);
+    }
+
+    metal_i2c_transfer(i2c, PCA9685_I2C_ADDRESS, bufWrite, 5, bufRead, 1);
 }
 
-void driveReverse(uint8_t speedFlag)
-{
-// your code from Milestone 1
+/*
+ * motor Reverse
+ * task 5: using bufWrite, bufRead, breakup(), and metal_i2c_transfer(), implement the function
+ * made above to Drive the motor backward. the given speedFlag will alter the motor speed as follows:
+ *
+ * speedFlag = 1 -> value to breakup = 267
+ * speedFlag = 2 -> value to breakup = 265 (optional)
+ * speedFlag = 3 -> value to breakup = 263 (optional)
+ *
+ * note 1: the motor's speed controller is either LED0 or LED1 depending on where its plugged into
+ * the board.4 if its LED1, simply add  to the LED0 address or type and replace LED1 with LED0
+ * ex: driveReverse(1);
+ */
+
+void driveReverse(uint8_t speedFlag){
+
+    bufWrite[0] = PCA9685_LED0_ON_L ;
+    bufWrite[1] = 0;
+    bufWrite[2] = 0;
+
+    if (speedFlag == 1) {
+
+        breakup(267, &bufWrite[3], &bufWrite[4]);
+
+    } else if (speedFlag == 2) {
+
+        breakup(265, &bufWrite[3], &bufWrite[4]);
+
+    } else if (speedFlag == 3) {
+
+        breakup(263, &bufWrite[3], &bufWrite[4]);
+    }
+
+    metal_i2c_transfer(i2c, PCA9685_I2C_ADDRESS, bufWrite, 5, bufRead, 1);
 }
 
-void raspberrypi_int_handler(int devid, int * angle, int * speed, int * duration)
+int raspberrypi_int_handler(int devid, int * angle, int * speed, int * duration)
 {
-    char * str = malloc(20 * sizeof(char)); // you can use this to store the received string
-                                            // it is the same as char str[20]                
-
-   // Extract the values of angle, speed and duration inside this function
-   // And place them into the correct variables that are passed in
-
+    char * str = malloc(50 * sizeof(char));
+    int serial;
+   
+    do {
+        serial = ser_readline(devid, 50, str);
+        if (serial > 0) {
+            sscanf(str, "%d %d %d", angle, speed, duration);
+            printf("Received values: angle=%d, speed=%d, duration=%d\n", *angle, *speed, *duration);
+        }
+    } while (serial > 0);
+   
     free(str);
+    return serial;
 }
 
 
@@ -109,26 +238,46 @@ int main()
     stopMotor();
     delay(2000);
 
-    // initialize UART channels
+    // Initialize UART channels
     ser_setup(0); // uart0 (receive from raspberry pi)
-    
+    ser_setup(1);
+
     printf("Setup completed.\n");
     printf("Begin the main loop.\n");
-    
 
-    int angle, speed, duration;
     // Drive loop
-    while (1) {
-        // The following pseudo-code is a rough guide on how to write your code
-        // It is NOT actual C code
-         /*  
-          if (is UART0 ready?)
-          {
-              call raspberrypi_int_handler() to get values of angle, speed and duration
+    int angle, speed, duration;
+    while (1)
+    {
+        if (ser_isready(1))
+        {
+            printf("Data is available\n");
 
-              call steering(), driveForward/Reverse() and delay with the extracted values
-          }
-        */
+            int serial = raspberrypi_int_handler(1, &angle, &speed, &duration);
+
+            if (-4 < speed && speed < 0)
+            {
+                driveReverse(speed);
+            }
+            else if (4 > speed && speed > 0)
+            {
+                driveForward(speed);
+            }
+            else
+            {
+                stopMotor();
+                printf("Motor stopped\n");
+            }
+
+            delay(duration * 1000);
+        }
+        else
+        {
+            printf("No data available\n");
+            delay(50); // Wait for 50ms before checking again
+        }
     }
+
     return 0;
 }
+
